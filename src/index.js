ad88d9ccf53e656ae8a791c4921cb2b8a7d4adb7
Nathan Robertson <nathanrobertson1997@gmail.com>
2025-04-09 02:28:17 +1000

    Set document title for React Notes App

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.js'
 
+// Set the document title.
+document.title = "React-Notes-App";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
